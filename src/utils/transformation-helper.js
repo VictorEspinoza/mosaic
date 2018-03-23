f48f6ce6491fa87eb0b310561b0031448957529e
victor espinoza <victor.espinoza@enrian.com>
2018-03-23 13:56:57 +0100

    - moves the transformation of an image into utils

--- src/utils/transformation-helper.js
+++ src/utils/transformation-helper.js
@@ -1,40 +1,60 @@
-const SIZE = 4;
-const NAMESPACE = "http://www.w3.org/2000/xmlns/";
-const XLINK = "xmlns:xlink";
-const W3C = "http://www.w3.org/1999/xlink";
-
-export const getColor = (imageData) => {
-  const rgbToHex =(r, g, b) => {
-    if (r > 255 || g > 255 || b > 255)
-      throw "Invalid color component";
-    return ((r << 16) | (g << 8) | b).toString(16);
-  };
-
-  const hex = "#" + ("000000" + rgbToHex(imageData[0], imageData[1], imageData[2])).slice(-6);
-  return hex;
-};
+const SIZE = 16;
 
 export const getUnitsPerDimension = (dimensionSize) => {
   return Math.ceil(dimensionSize / SIZE);
 };
 
+export const transform = (width, height, canvas, context) => {
+  const svg = prepareMainSvg(width, height);
+  return createMosaic(width, height, canvas, context, svg);
+};
+
 const prepareMainSvg = (width, height) => {
   const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
   svg.setAttribute('style', 'border: 1px solid black');
-  svg.setAttribute('width', '600');
-  svg.setAttribute('height', '250');
+  svg.setAttribute('width', width);
+  svg.setAttribute('height', height);
 
   return svg;
 };
 
-const prepareCircle = (svgNs, coordinateX, coordinateY, color) => {
-  const circle = document.createElementNS(svgNS, 'circle');
-   circle.setAttribute('cx', coordinateX);
-   circle.setAttribute('cy', coordinateY);
-   circle.setAttribute('r', SIZE);
-   circle.setAttribute('fill', color);
+const rgbToHex =(r, g, b) => {
+  if (r > 255 || g > 255 || b > 255)
+    throw "Invalid color component";
+  return ((r << 16) | (g << 8) | b).toString(16);
+};
 
-   return circle;
+const createMosaic = (width, height, canvas, context, svg) => {
+  const svgNS = svg.namespaceURI;
+  const rows = getUnitsPerDimension(height);
+  const cols = getUnitsPerDimension(width);
+
+  let yCoordinate = 0;
+  let xCoordinate = 0;
+
+  for (let i = 0; i <= cols; i++) {
+    for (let j = 0; j <= rows; j++) {
+      const data = context.getImageData(yCoordinate, xCoordinate, SIZE, SIZE).data;
+      const colorHex = "#" + ("000000" + rgbToHex(data[0], data[1], data[2])).slice(-6);
+      const circle = prepareCircle(svgNS, yCoordinate, xCoordinate, colorHex);
+      svg.appendChild(circle);
+      xCoordinate += SIZE;
+    }
+    yCoordinate += SIZE;
+    xCoordinate = 0;
+  }
+  return svg;
+};
+
+
+const prepareCircle = (svgNS, yCoordinate, xCoordinate, colorHex) => {
+  const circle = document.createElementNS(svgNS, 'circle');
+  circle.setAttribute('cx', yCoordinate);
+  circle.setAttribute('cy', xCoordinate);
+  circle.setAttribute('r', SIZE / 2);
+  circle.setAttribute('fill', colorHex);
+
+  return circle;
 };
 
 
